bf89725cad66f598a7db3be57ff85c9ec4853529
akhanfrontend <163712119+akhanfrontend@users.noreply.github.com>
2026-02-16 02:56:18 -0800

    script.js

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,1 +1,21 @@
 
+// adding add function and alert
+
+document.getElementById("btn").onclick = function () {
+
+    let val = document.getElementById("inputBox").value;
+
+    if(val === ""){
+        alert ("Request you to please add task")
+        return;
+    }
+    let li = document.createElement("li");
+
+    li.innerText = val;
+    
+
+    document.getElementById("taskList").appendChild(li);
+    
+
+    document.getElementById("inputBox").value = "";
+}
